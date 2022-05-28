8d56112bdd4dbd0cf505a8f561d67e9b54de0ce0
igormgomes <igormgomes94@gmail.com>
2022-05-28 12:42:37 -0300

    feat - add post payment

--- payment-service/src/main/kotlin/br/com/developers/payment/EventType.java
+++
@@ -1,5 +0,0 @@
-package br.com.developers.payment;
-
-public enum EventType {
-    PROCESSED_PAYMENT, SCHEDULED_PAYMENT
-}
---
+++ payment-service/src/main/kotlin/br/com/developers/payment/EventType.kt
@@ -0,0 +1,5 @@
+package br.com.developers.payment
+
+enum class EventType {
+    PROCESSED_PAYMENT, SCHEDULED_PAYMENT
+}
